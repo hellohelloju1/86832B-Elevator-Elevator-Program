d42b55736100e970b650745092caa887579dfc64
Patrick Young <101240625+hellohelloju1@users.noreply.github.com>
2026-05-21 21:50:34 +0800

    added include/rawr.h

---
+++ include/rawr.h
@@ -0,0 +1,12 @@
+#include "api.h"
+namespace rawr {
+
+    extern bool isLeft;
+    extern bool isBlue;
+    extern int LIMIT_TOP;
+    extern int LIMIT_BOTTOMHAHA;
+    extern pros::Controller control;
+    extern pros::Motor MotorLeft;
+    extern pros::Motor MotorRight;
+    extern pros::Motor intake;    
+}
